0f67af94b9d6ed32121f624fcc36b1c3d1941289
OrestisL <liaskos.orestis@gmail.com>
2023-03-28 14:28:53 +0300

    service checker not working properly (check) application crashes when write freq too low

--- Assets/Scripts/UnitySQLite/AccountManagement.cs
+++ Assets/Scripts/UnitySQLite/AccountManagement.cs
@@ -46,7 +46,7 @@ namespace UnitySQLite
         {
             base.Awake();
             //consider added a check so as not to do this over and over
-            //Account.CreateDefaultAccounts(); -> do this on database creation
+            //Account.CreateDefaultAccounts(); -> do this on database creation done
             // onLoginAttempt += (b, text) => output.text = text;
             onSuccessfulLogin += (acc) => { CurrentAccount = acc; };
 
