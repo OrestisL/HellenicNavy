448d2c2e654bfce5bcf26691a87eceb0989ec515
OrestisL <liaskos.orestis@gmail.com>
2023-03-28 17:02:17 +0300

    start of report tests

---
+++ Assets/Scripts/Tests/pdfTests.cs
@@ -0,0 +1,12 @@
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using UnityEngine;
+using TMPro;
+
+public class pdfTests : MonoBehaviour
+{
+    public TMP_InputField inputField;
+
+}
