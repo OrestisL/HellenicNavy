7b27ad3105e179c752e764fef1a415d5b895313d
OrestisL <liaskos.orestis@gmail.com>
2023-04-06 15:44:09 +0300

    report: fix containers for headers start of table creation

--- Assets/Scripts/Tests/pdfTests.cs
+++ Assets/Scripts/Tests/pdfTests.cs
@@ -6,9 +6,9 @@ using PdfSharpCore.Pdf.IO;
 using TMPro;
 using System.IO;
 using System.Text;
-using System.Diagnostics;
 using PdfSharpCore.Drawing.Layout;
 using System;
+using Unity.VisualScripting;
 
 public class pdfTests : MonoBehaviour
 {
@@ -36,24 +36,21 @@ public class pdfTests : MonoBehaviour
             XTextFormatter textFormatter = new XTextFormatter(gfx);
 
             // Create a font
-            XFont font = new XFont("Times New Roman", fontSize, XFontStyle.Regular);
+            XFont font = new XFont("Verdana", fontSize, XFontStyle.Regular);
 
             //add an image
             //DrawImageOriginalSize(gfx, page, SettingsHolder.Instance.settings.FullPath);
             //Vector2 imgSize = DrawImageScaled(gfx, page, SettingsHolder.Instance.settings.BadgeFullPath, 150, 150);
-            (double maxW, double maxH) = CreateHeaderTemplate(gfx, page, 150);
+            (double maxW, double maxH) = CreateHeaderTemplate(gfx, page, 75);
             //create the text rect
-            XRect textRect = new XRect(margin / 2, margin / 2 + maxH, page.Width - margin, page.Height - margin);
-            //gfx.DrawRectangle(XBrushes.AntiqueWhite, textRect);
+            XRect textRect = new XRect(margin, margin / 2 + maxH, page.Width - 2 * margin, page.Height - 2 * margin);
+            gfx.DrawRectangle(XBrushes.AntiqueWhite, textRect);
             textFormatter.Alignment = XParagraphAlignment.Justify;
             // Draw the text
-            //textFormatter.DrawString(s, font, XBrushes.Black, textRect, XStringFormats.TopLeft);
+            textFormatter.DrawString(s, font, XBrushes.Black, textRect, XStringFormats.TopLeft);
 
-            // Save the document...            
-            const string filename = "HelloWorld.pdf";
-            document.Save(filename);
-            // ...and start a viewer.
-            //Process.Start(filename);
+            // Save the document           
+            SavePdfDocument(document);
         });
     }
 
@@ -82,6 +79,7 @@ public class pdfTests : MonoBehaviour
         //load images
         XImage badgeImg = XImage.FromFile(SettingsHolder.Instance.settings.BadgeFullPath);
         XImage hnImg = XImage.FromFile(SettingsHolder.Instance.settings.HNFullPath);
+        string shipName = SettingsHolder.Instance.settings.shipName;
 
         //get max for normalized scaling 
         double maxSizeBadge = Math.Max(badgeImg.Size.Width, badgeImg.Size.Height);
@@ -98,26 +96,93 @@ public class pdfTests : MonoBehaviour
         //badge top left, HN top right
         gfx.DrawImage(badgeImg, margin / 2, margin / 2, finalWidthBadge, finalHeightBadge);
         gfx.DrawImage(hnImg, page.Width - finalWidthHN - margin / 2, margin / 2, finalWidthHN, finalHeightHN);
-
         //draw text between images
         XTextFormatter tf = new XTextFormatter(gfx);
         tf.Alignment = XParagraphAlignment.Center;
-        XFont font = new XFont("Times New Roman", 25, XFontStyle.Bold);
-        double verticalPos = 50 + margin;
-        XRect titleRect = new XRect(0, verticalPos, page.Width, 25);
+        XFont font = new XFont("Verdana", 25, XFontStyle.Bold);
+        double verticalPos = margin - 5;
+        double horzSize = page.Width - finalWidthHN - finalWidthBadge - 2 * margin;
+        XRect titleRect = new XRect(finalWidthBadge + margin, verticalPos, horzSize, 25);
+        gfx.DrawRectangle(XBrushes.Black, titleRect);
         tf.Alignment = XParagraphAlignment.Center;
-        tf.DrawString(SettingsHolder.Instance.settings.shipName, font, XBrushes.Black, titleRect);
-        font = new XFont("Times New Roman", 18, XFontStyle.Regular);
-        verticalPos += 25;
-        tf.DrawString("ΑΝΑΦΟΡΑ ΕΠΙΣΚΕΥΩΝ", font, XBrushes.Black, new XRect(0, verticalPos, page.Width, 18));
-        font = new XFont("Times New Roman", 16, XFontStyle.Regular);
+        tf.DrawString(shipName, font, XBrushes.Black, titleRect);
+        font = new XFont("Verdana", 18, XFontStyle.Regular);
         verticalPos += 30;
-        tf.DrawString(string.Format("Ημερομηνία {0}", DateTime.Now.ToString("dd-MM-yy")), font, XBrushes.Black, new XRect(0, verticalPos, page.Width, 16));
-
+        titleRect = new XRect(finalWidthBadge + margin, verticalPos, horzSize, 18);
+        gfx.DrawRectangle(XBrushes.Black, titleRect);
+        tf.DrawString("ΑΝΑΦΟΡΑ ΕΠΙΣΚΕΥΩΝ", font, XBrushes.Black, titleRect);
+        font = new XFont("Verdana", 16, XFontStyle.Regular);
+        verticalPos += 20;
+        titleRect = new XRect(finalWidthBadge + margin, verticalPos, horzSize, 16);
+        gfx.DrawRectangle(XBrushes.Black, titleRect);
+        tf.DrawString(string.Format("Ημερομηνία {0}", DateTime.Now.ToString("dd-MM-yy")), font, XBrushes.Black, titleRect);
 
         return (Math.Max(finalWidthHN, finalWidthBadge) + margin / 2, Math.Max(finalHeightBadge, finalHeightHN) + margin / 2);
     }
 
+    void SavePdfDocument(PdfDocument document)
+    {
+        string reportsPath = Path.Combine(Directory.GetCurrentDirectory(), "Αναφορές");
+        if (!Directory.Exists(reportsPath))
+        {
+            Directory.CreateDirectory("Αναφορές");
+        }
+
+        //check if file already exists
+        string filename = string.Format("Αναφορά {0}", DateTime.Now.ToString("dd-MM-yy"));
+        //first time creating the file it should not exist
+        if (!File.Exists(Path.Combine(reportsPath, string.Format("{0}.pdf", filename))))
+        {
+            document.Save(Path.Combine(reportsPath, string.Format("{0}.pdf", filename)));
+            Debug.Log(string.Format("Successfully saved report {0} at {1}", filename, Path.Combine(reportsPath, filename)));
+            return;
+        }
+        //if multiple files are saved within the same day, check and add a (#) at the end
+        else
+        {
+            for (int i = 1; ; i++)
+            {
+                string currentName = string.Format("{0} ({1})", filename, i);
+                if (!File.Exists(Path.Combine(reportsPath, string.Format("{0}.pdf", currentName))))
+                {
+                    document.Save(Path.Combine(reportsPath, string.Format("{0}.pdf", currentName)));
+                    Debug.Log(string.Format("Successfully saved report {0} at {1}.pdf", currentName, Path.Combine(reportsPath, currentName)));
+                    return;
+                }
+            }
+
+        }
+    }
 
 
+    void CreateTableInDocument(XGraphics gfx, PdfPage page, int amountElements, double offsetX, double offsetY)
+    {
+        // Text format
+        XStringFormat format = new XStringFormat();
+        format.LineAlignment = XLineAlignment.Near;
+        format.Alignment = XStringAlignment.Near;
+        XTextFormatter tf = new XTextFormatter(gfx);
+
+        //fonts
+        XFont cellFont = new XFont("Verdana", 10, XFontStyle.Regular);
+        XFont headerFont = new XFont("Verdana", 11, XFontStyle.Bold);
+
+        //offset between lines
+        double lineHeight = 20;
+
+        //element element dimensions
+        int elementWidth = (int)(page.Width - margin) / 2;
+        int doubleElementWidth = 2 * elementWidth;
+        int elementHeight = 120; //consider changing?
+
+        int lineOffset = 1;
+        int doubleLineOffset = 2 * lineOffset;
+
+        //color of squares
+        XSolidBrush rect_style1 = new XSolidBrush(XColors.White);
+
+        //first draw the background black rect
+        gfx.DrawRectangle(XBrushes.Black, offsetX, offsetY, doubleElementWidth + doubleLineOffset, amountElements * elementHeight);
+
+    }
 }
