29013fa9d998fc57474691b46b1395eac9110d18
OrestisL <liaskos.orestis@gmail.com>
2023-04-07 15:08:43 +0300

    created cells with headers

--- Assets/Scripts/Tests/pdfTests.cs
+++ Assets/Scripts/Tests/pdfTests.cs
@@ -5,10 +5,8 @@ using PdfSharpCore.Pdf;
 using PdfSharpCore.Pdf.IO;
 using TMPro;
 using System.IO;
-using System.Text;
 using PdfSharpCore.Drawing.Layout;
 using System;
-using Unity.VisualScripting;
 using System.Collections.Generic;
 
 public class pdfTests : MonoBehaviour
@@ -25,7 +23,7 @@ public class pdfTests : MonoBehaviour
         {
             // Create a new PDF document
             PdfDocument document = new PdfDocument();
-            document.Info.Title = "Report xx-xx-xx";
+            //document.Info.Title = "Report xx-xx-xx";
 
             // Create an empty page
             PdfPage page = document.AddPage();
@@ -51,10 +49,10 @@ public class pdfTests : MonoBehaviour
             //textFormatter.DrawString(s, font, XBrushes.Black, textRect, XStringFormats.TopLeft);
             Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>
             {
-                { "test", new List<string> { "test", "test1", "" } },
-                { "test2", new List<string> { "test", "test1", "" } },
-                { "test3", new List<string> { "test", "test1", "" } },
-                { "test4", new List<string> { "test", "test1", "" } }
+                { "test1", new List<string> { "test", "test1", "test3", "test", "test1", "test3" } },
+                { "test2", new List<string> { "test", "test1", "test3", "test", "test1", "test3" } },
+                { "test3", new List<string> { "test", "test1", "test3", "test", "test1", "test3" } },
+                { "test4", new List<string> { "test", "test1", "test3", "test", "test1", "test3" } }
             };
             CreateTableInDocument(gfx, page, margin, maxH + 10, dict);
             // Save the document           
@@ -174,20 +172,20 @@ public class pdfTests : MonoBehaviour
         XTextFormatter tf = new XTextFormatter(gfx);
 
         //fonts
-        XFont cellFont = new XFont("Verdana", 10, XFontStyle.Regular);
-        XFont headerFont = new XFont("Verdana", 11, XFontStyle.Bold);
+        XFont cellFont = new XFont("Verdana", 6, XFontStyle.Regular);
+        XFont headerFont = new XFont("Verdana", 7, XFontStyle.Bold);
 
         //element dimensions
-        int elementWidth = (int)(page.Width - margin) / 2;
+        int elementWidth = (int)(page.Width - margin) / 2; //282
         int doubleElementWidth = 2 * elementWidth;
-        //TODO change height according to how many lines there are per machinery, because 120 is too big
-        //or ask if it looks ok
-        int elementHeight = 120;    //consider changing? with this height page can hold a table of 6x2 cells
-                                    //add new page after 6
-                                    //also need new page for comments
+        ////TODO change height according to how many lines there are per machinery, because 120 is too big
+        ////or ask if it looks ok
+        int elementHeight = 40; //consider changing? with this height page can hold a table of 6x2 cells
+                                //add new page after 6
+                                //also need new page for comments
 
         //offset between lines
-        int lineOffset = 3;
+        int lineOffset = 2;
         int doubleLineOffset = 2 * lineOffset;
 
         //color of squares
@@ -197,34 +195,77 @@ public class pdfTests : MonoBehaviour
         int amountElements = contents.Keys.Count; //keys will be machinery names
         gfx.DrawRectangle(XBrushes.Black, offsetX - lineOffset, offsetY, doubleElementWidth + doubleLineOffset + lineOffset - margin, amountElements * (elementHeight + lineOffset) + lineOffset);
 
-        int i = 0;
+        int i = -1;
         foreach (KeyValuePair<string, List<string>> item in contents)
         {
-            double currentYOffset = offsetY + lineOffset * (i + 1) + elementHeight * i;
-            //create a white box for the machinery
-            XRect nameRect = new XRect(margin, currentYOffset, elementWidth - margin / 2, elementHeight);
+            double currentYOffset = offsetY + lineOffset * (i + 2) + elementHeight * (i + 1);
+            double currentXOffset = margin;
+            //table should probably be like this
+            //  NAME    ID  SYSTEM  DEPT    ServiceDescr    Status
+            //  82      40  50      20      290             82        (widths)
+            //used to be 2 cells 282x120 each = 564x120 total (check into these)
+            double nameWidth = 82;
+            double idWidth = 42;
+            double systemWidth = 50;
+            double deptWidth = 20;
+            double serviceDescrWidth = 252;
+            double statusWidth = 82;
+
+            //name box
+            XRect nameRect = new XRect(currentXOffset, currentYOffset, nameWidth, elementHeight);
             gfx.DrawRectangle(rectStyle, nameRect);
-            //create a white box for all the info
-            XRect infoRect = new XRect(margin / 2 + elementWidth + lineOffset, currentYOffset, elementWidth - margin / 2, elementHeight);
-            gfx.DrawRectangle(rectStyle, infoRect);
+            //id box
+            currentXOffset += nameWidth + lineOffset;
+            XRect idRect = new XRect(currentXOffset, currentYOffset, idWidth, elementHeight);
+            gfx.DrawRectangle(rectStyle, idRect);
+            //system box
+            currentXOffset += idWidth + lineOffset;
+            //XRect systemRect = new XRect(currentXOffset, currentYOffset, systemWidth, elementHeight);
+            //gfx.DrawRectangle(rectStyle, systemRect);
+            ////dept box
+            //currentXOffset += systemWidth + lineOffset;
+            //XRect deptRect = new XRect(currentXOffset, currentYOffset, deptWidth, elementHeight);
+            //gfx.DrawRectangle(rectStyle, deptRect);
+            XRect deptAndSystemRect = new XRect(currentXOffset, currentYOffset, deptWidth + systemWidth, elementHeight);
+            gfx.DrawRectangle(rectStyle, deptAndSystemRect);
+            //service box
+            currentXOffset += systemWidth + deptWidth + lineOffset;
+            XRect serviceDescrRect = new XRect(currentXOffset, currentYOffset, serviceDescrWidth, elementHeight);
+            gfx.DrawRectangle(rectStyle, serviceDescrRect);
+            //status box
+            currentXOffset += serviceDescrWidth + lineOffset;
+            XRect statusRect = new XRect(currentXOffset, currentYOffset, statusWidth, elementHeight);
+            gfx.DrawRectangle(rectStyle, statusRect);
+
 
             //write inside boxes
+            if (i == -1)
+            {
+                tf.DrawString("ΟΝΟΜΑ ΜΗΧΑΝΗΜΑΤΟΣ", headerFont, XBrushes.Black, nameRect);
+                tf.DrawString("ΚΩΔΙΚΟΣ", headerFont, XBrushes.Black, idRect);
+                //tf.DrawString("ΣΥΣΤΗΜΑ", headerFont, XBrushes.Black, systemRect);
+                //tf.DrawString("ΕΠΙΣΤΑΣΙΑ", headerFont, XBrushes.Black, deptRect);
+                tf.DrawString("ΣΥΣΤΗΜΑ ΚΑΙ\nΕΠΙΣΤΑΣΙΑ", headerFont, XBrushes.Black, deptAndSystemRect);
+                tf.DrawString("ΠΕΡΙΓΡΑΦΗ ΕΠΙΣΚΕΥΗΣ", headerFont, XBrushes.Black, serviceDescrRect);
+                tf.DrawString("ΚΑΤΑΣΤΑΣΗ", headerFont, XBrushes.Black, statusRect);
+                i++;
+                continue;
+            }
+
             //machinery box
-            tf.DrawString(item.Key, cellFont, XBrushes.Black, nameRect);
+            tf.DrawString(item.Key, cellFont, XBrushes.Black, nameRect, format);
             //info box
             string infos = string.Empty;
-            for (int j  = 0; j < item.Value.Count; j++)
+            for (int j = 0; j < item.Value.Count; j++)
             {
                 infos += string.Format("{0}\n", item.Value[j]);
             }
             infos.Remove(infos.Length - 2, 1);
-            tf.DrawString(infos, cellFont, XBrushes.Black, infoRect);
+            tf.DrawString(infos, cellFont, XBrushes.Black, serviceDescrRect, format);
             i++;
         }
 
-        //table should probably be like this
-        //NAME|ID|SYSTEM|DEPT|ServiceDescr|Completed
 
-        
+
     }
 }
